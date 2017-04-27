7687e639bdb0cddfc3cc683567cd6cde9f6b39ef
John Fearnside <jfearnside@avvo.com>
2017-04-27 12:20:27 -0700

    Add push script and README

---
+++ push.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+VERSION=$1
+if [ -z ${VERSION} ]; then
+  echo "Please provide a Docker image version tag"
+  exit 1
+fi
+
+set -x
+
+IMAGE="avvo/$(basename $(pwd))"
+
+docker build -t "${IMAGE}:${VERSION}" -t "${IMAGE}:latest" .
+docker push "${IMAGE}:${VERSION}"
+docker push "${IMAGE}:latest"
